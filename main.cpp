5798a122da0b1e3ba7f8fcc7e66471426a80fac1
Dominik Schweigl <domnik.schweigl@gmail.com>
2026-03-23 20:41:51 +0100

    chore: create SFML hello world

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Metroidvania Game");
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color::Green);
+
+    while(window.isOpen()) {
+        while(const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
