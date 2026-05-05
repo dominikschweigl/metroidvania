fa6a53c62201d0c6276e6f22d7ba3008d1015ade
Emanuel Andrä Schöpf <emanuel.a.schoepf@student.uibk.ac.at>
2026-05-05 18:56:44 +0200

    chore: renaming loop counter

--- src/entities/base/entity_physics.cpp
+++ src/entities/base/entity_physics.cpp
@@ -68,7 +68,7 @@ void simulateMovement(float deltaTime, sf::Vector2f &position, sf::Vector2f &vel
 	applyGravity(velocity.y, isOnGround, deltaTime, gravity,
 	             sf::FloatRect({position.x - width / 2.f, position.y - height}, {width, height}), world);
 	static constexpr float step = 0.01f;
-	for (float t = 0.f; t < deltaTime; t += step) {
+	for (float counter = 0.f; counter < deltaTime; counter += step) {
 		position.x = resolveHorizontal(position, velocity.x, width, height, step, world);
 		position.y = resolveVertical(position, velocity.y, isOnGround, width, height, step, world);
 	}
