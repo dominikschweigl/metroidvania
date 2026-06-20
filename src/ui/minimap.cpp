ad4ee8b055b8374ed40988e45b6f9ce7e532c0a8
Emanuel Andrä Schöpf <emanuel.a.schoepf@student.uibk.ac.at>
2026-06-20 15:49:51 +0200

    chore: cleaned up comments

--- src/ui/minimap.cpp
+++ src/ui/minimap.cpp
@@ -14,7 +14,6 @@ void MiniMap::draw(sf::RenderWindow &window, const sf::Vector2f playerPos, const
 	sf::Vector2f minimapPos =
 	    sf::Vector2f({window.getSize().x - texSize.x - 5.f, window.getSize().y - texSize.y - 5.f});
 	sprite.setPosition(minimapPos);
-	// sprite.setPosition({0, 0});
 
 	sf::CircleShape playerCircle;
 	playerCircle.setRadius(4.f);
@@ -22,25 +21,12 @@ void MiniMap::draw(sf::RenderWindow &window, const sf::Vector2f playerPos, const
 	if (room_in_minimap.contains(room_id)) {
 		sf::Vector2f relativePlayerPos = sf::Vector2f(
 		    {playerPos.x / (room.width * World::TILE_SIZE), playerPos.y / (room.height * World::TILE_SIZE)});
-		// playerCircle.setPosition(
-		//     {minimapPos.x + (room_in_minimap.at(room_id).position.x + relativePlayerPos.x) * (texSize.x - 5.f),
-		//      minimapPos.y + (room_in_minimap.at(room_id).position.y + relativePlayerPos.y) * (texSize.y - 5.f)});
 
 		const auto &mapRoom = room_in_minimap.at(room_id);
 
 		playerCircle.setPosition(
 		    {minimapPos.x + (mapRoom.position.x + relativePlayerPos.x * mapRoom.size.x) * (texSize.x - 5.f),
 		     minimapPos.y + (mapRoom.position.y + relativePlayerPos.y * mapRoom.size.y) * (texSize.y - 5.f)});
-
-		// playerCircle.setPosition(
-		//     {minimapPos.x
-		//          + (room_in_minimap.at(room_id).position.x + relativePlayerPos.x +
-		//          room_in_minimap.at(room_id).size.x)
-		//                * (texSize.x - 5.f),
-		//      minimapPos.y
-		//          + (room_in_minimap.at(room_id).position.y + relativePlayerPos.y +
-		//          room_in_minimap.at(room_id).size.y)
-		//                * (texSize.y - 5.f)});
 	} else {
 		std::cout << "Roomid: " << room_id << " does not exist" << std::endl;
 		playerCircle.setPosition({minimapPos.x, minimapPos.y});
